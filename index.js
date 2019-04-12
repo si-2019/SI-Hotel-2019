bd5c70e0615436418f61dd5d8ffe9908ae03d965
hsoljic1 <hsoljic1@etf.unsa.ba>
2019-04-12 23:04:42 +0200

    init db

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
+var mysql = require('mysql')
+let con = mysql.createConnection({
+    host: 'remotemysql.com',
+    user: 'TYQcLL35gV',
+    password: 'BLysSj9ZrP',
+    database: 'TYQcLL35gV'
+})
+con.connect()
+
+
 var express = require('express')
 var app = express()
 app.listen(process.env.PORT || 9123)
@@ -7,11 +17,18 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require('./requests/aminaReqs.js')(app)
-require('./requests/hanaReqs.js')(app)
-require('./requests/harisReqs.js')(app)
-require('./requests/nejiraReqs.js')(app)
-require('./requests/rijadReqs.js')(app)
+require('./requests/aminaReqs.js')(app, con)
+require('./requests/hanaReqs.js')(app, con)
+require('./requests/harisReqs.js')(app, con)
+require('./requests/nejiraReqs.js')(app, con)
+require('./requests/rijadReqs.js')(app, con)
+
+
+
+con.query('select * from Anketa', (err, res) => {
+    console.log(err, res[0])
+})
+
 
 module.exports = app
 
--- requests/harisReqs.js
+++ requests/harisReqs.js
@@ -1,4 +1,4 @@
-module.exports = function(app) {
+module.exports = function(app, con) {
     /* ovdje se pišu zahtjevi npr.
       app.get('/hotel', function(req, res) {
         res.send('hotel - ankete')  
