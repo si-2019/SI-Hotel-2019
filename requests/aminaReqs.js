b931c534df1bf1f88e2d99be9881ca927b6febe4
Amina Salcin <asalcin2@etf.unsa.ba>
2019-05-04 22:30:15 +0200

    Kreiranje rute za pribavljanje informacija o tipu ankete i kreiranje rute za pribavljanje informacija o predmetu za koji je kreirana

--- requests/aminaReqs.js
+++ requests/aminaReqs.js
@@ -10,5 +10,34 @@ module.exports = function(app, con) {
       }
       res.json({datumKreiranja: result[0].datumKreiranja});
   })
+}) 
+app.get('/getTipAnkete', function(req, res) {
+  con.query('SELECT tipAnkete FROM Anketa WHERE idAnketa = ' + req.query.idAnketa, function(error, result){
+    if (error)
+    {
+      res.json({message: error});
+      return;
+    }
+    res.json({tipAnkete: result[0].tipAnkete});
+  })
 })
+
+app.get('/getPredmet', function(req, res) {
+  con.query('SELECT idPredmet FROM Anketa WHERE idAnketa = ' + req.query.idAnketa, function(error, result){
+    if (error)
+    {
+      res.json({message: error});
+      return;
+    }
+    con.query('SELECT naziv FROM Predmet WHERE id = ' + result[0].idPredmet, function(error, result){
+      if (error)
+      {
+        res.json({message: error});
+        return;
+      }
+      res.json({nazivPredmeta: result[0].naziv});
+    })
+  })
+})
+
 }
