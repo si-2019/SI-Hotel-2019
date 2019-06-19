40fa390ed97a5a581b1d0a433d4b8290b9b5c5cb
hsoljic1 <37188708+hsoljic1@users.noreply.github.com>
2019-06-19 11:16:17 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,11 +1,8 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("TYQcLL35gV", "TYQcLL35gV", "BLysSj9ZrP", {
-  host: "mysql-3213-0.cloudclusters.net",
-  dialect: "mysql",
-  port: 10021,
-  logging: false
+const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP",{host:'mysql-3213-0.cloudclusters.net',dialect:"mysql",logging:console.log,  port: 10021,define: {
+        timestamps: false
+    }
 });
-
 const db = {}
 db.Sequelize = Sequelize;  
 db.sequelize = sequelize;
@@ -18,7 +15,6 @@ db.odgovor = sequelize.import(__dirname+'/Odgovor');
 db.popunjenaAnketa = sequelize.import(__dirname+'/PopunjenaAnketa');
 db.predmet = sequelize.import(__dirname+'/Predmet');
 db.korisnik = sequelize.import(__dirname+'/Korisnik');
-db.predmet_student = sequelize.import(__dirname + '/predmet_student')
 
 
 module.exports=db;
